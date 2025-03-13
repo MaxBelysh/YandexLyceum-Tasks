7bf12df067bf76b09bd45f4aae6041124096bc51
Беляков Максим <max.maxmin.maxtag@gmail.com>
2025-03-13 19:27:30 +0300

    Add files via upload

--- mars/ts.py
+++ mars/ts.py
@@ -36,5 +36,31 @@ def img_mars():
                    </html>'''
 
 
+@app.route("/promotion_image")
+def promotion_image():
+    return f'''<!doctype html>
+                       <html lang="en">
+                         <head>
+                           <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                            <link rel="stylesheet" 
+                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                            crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href='/static/css/style.css'>
+                           <title>Колонизация</title>
+                         </head>
+                         <body>
+                           <h1>Жди нас Марс!</h1>
+                           <img src="/static/img/mars.png" alt="здесь должна быть картинка">
+                           <div>Человечество вырастает из детства.</div>
+                           <div>Человечеству мала одна планета.</div>
+                           <div>Мы сделаем обитаемыми безжизненные пока планеты.</div>
+                           <div>И начнем с Марса!</div>
+                           <div>Присоединяйся!</div>
+                         </body>
+                       </html>'''
+
+
 if __name__ == "__main__":
     app.run(port=8080, host="127.0.0.1")
